5dec9c7cd01bc97c9a9eb98892824dc12aaf313e
Phillip <phillipables1@gmail.com>
2018-03-24 10:13:42 -0400

    main activity java declare drawerlayout and actionbardrawertoggle variables then set drawerlayout by id in oncreate and mtoggle as new toggle this, mDrawerlayout, 'open', 'close'

--- app/src/main/java/com/example/techtron/drawerlayout/MainActivity.java
+++ app/src/main/java/com/example/techtron/drawerlayout/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.techtron.drawerlayout;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, "Open", "Close");
+
     }
 }
